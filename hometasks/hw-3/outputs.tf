46ee5d0d2d9c7d421d7bf51b1bae551e3edfdd44
Meder <mederemilev04@gmail.com>
2026-01-15 17:33:17 -0600

    hw-4

--- hometasks/hw-3/outputs.tf
+++ hometasks/hw-3/outputs.tf
@@ -2,3 +2,15 @@
 output "public_subnet_cidr" {
   value = aws_subnet.public[*].cidr_block
 }
+
+output "vpc_id" { 
+  value = aws_vpc.main.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private[*].id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public[*].id
+}
